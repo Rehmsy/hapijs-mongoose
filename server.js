a940639ee259bd1245462dbb0ad44898e02751ec
Rehmsy <sarahrehmer@gmail.com>
2018-08-12 21:06:48 -0700

    stuff

---
+++ server.js
@@ -0,0 +1,64 @@
+const Hapi = require('hapi');
+const Mongoose = require('mongoose');
+const Wolf = require('./lib/models/wolf');
+//const port = process.env.PORT || 4000;
+
+Mongoose.connect('mongodb://localhost:27017/switch');
+Mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+const server = Hapi.Server({  
+    host: 'localhost',
+    port: 4000
+});
+
+server.route([
+    {
+        method: 'GET',
+        path: '/',
+        handler: function(request) {
+            return 'Hello, dumb world!';
+        }
+    },
+    {
+        method: 'GET',
+        path: '/api',
+        handler: function(request) {
+            return 'Hello dumb Hapi API!';
+        }
+    },
+    {
+        method: 'GET',
+        path: '/api/wolves',
+        handler: function(request) {
+            return 'Hello dumb Hapi Wolf API!';
+        }
+    },
+    {
+        method: 'POST',
+        path: '/api/wolves',
+        handler: (req, reply) => {
+            const { name } = req.payload;
+            const wolf = new Wolf({
+                name
+            });
+
+            return reply.response(wolf).code(200);
+        }
+    }
+]);
+
+const init = async() => {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+module.exports = server;
---
+++ test/unit/wolves.test.js
@@ -0,0 +1,44 @@
+const chai = require('chai');
+const { assert } = chai;
+const Wolf = require('../../lib/models/wolf');
+//const { getErrors } = require('./helpers');
+
+
+describe('Game model', () => {
+
+    it('validates good model', () => {
+        const data = {
+            name: 'Wolf Face', 
+        };
+        const wolf = new Wolf(data);
+
+        const json = wolf.toJSON();
+        delete json._id;
+        assert.deepEqual(json, data);
+        assert.isUndefined(wolf.validateSync());
+    });
+});
+
+
+
+
+// const Lab = require('lab');
+
+// const code = require('code');
+
+// const lab = exports.lab = Lab.script();
+// var server = require('../app.js').server;
+
+// lab.test('starter pack', function(done) {
+//     var options = {
+//         method: 'GET',
+//         url: '/api'
+//     };
+
+//     server.inject(options, function(response) {
+//         code.expect(response.statusCode).to.equal(200);
+//         code.expect(response.result).to.be.string();
+
+//         done();
+//     });
+// });
